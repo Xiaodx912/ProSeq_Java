29f5b4485a0ae2e3307129dedcbd5b8da07ada73
xiaodx <admin@hareru.moe>
2020-12-03 22:54:20 +0800

    .

---
+++ test/moe/hareru/proseq/OneHot_EncoderTest.java
@@ -0,0 +1,18 @@
+package moe.hareru.proseq;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OneHot_EncoderTest {
+    private static final Logger logger = LoggerFactory.getLogger(OneHot_EncoderTest.class);
+
+    @Test
+    void encode() {
+        logger.debug("Testing OneHot_Encoder.encode");
+        OneHot_Encoder enc=new OneHot_Encoder();
+        assertArrayEquals(new String[]{"00010000010000011001", "00100000010111000000", "10010000100000000100"},enc.Encode(new String[]{"STLYE", "QLDNP", "AEVKK"}));
+    }
+}
